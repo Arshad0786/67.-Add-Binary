3234903b077ac3bf40c9954a68a029afd1d57b7f
Arshad0786 <js07150715@gmail.com>
2020-05-24 21:39:10 +0800

    Updating code that is passed in leetcode

--- AddBinary.py
+++ AddBinary.py
@@ -1,3 +1,48 @@
 class Solution:
     def addBinary(self, a, b):
-        pass
+        Aindex = len(a) - 1
+        Bindex = len(b) - 1
+        carry = 0
+        output = ""
+        while Aindex >= 0 and Bindex >= 0:
+            if int(a[Aindex]) + int(b[Bindex]) + carry >= 2:
+                output = output + str(int(a[Aindex]) + int(b[Bindex]) + carry - 2)
+                carry = 1
+            else:
+                output = output + str(int(a[Aindex]) + int(b[Bindex]) + carry)
+                carry = 0
+            Aindex = Aindex - 1
+            Bindex = Bindex - 1
+
+        while Aindex >= 0:
+            if int(a[Aindex]) + carry == 2:
+                output = output + str(int(a[Aindex]) + carry - 2)
+                carry = 1
+            else:
+                output = output + str(int(a[Aindex]) + carry)
+                carry = 0
+            Aindex = Aindex - 1
+
+        while Bindex >= 0:
+            if int(b[Bindex]) + carry == 2:
+                output = output + str(int(b[Bindex]) + carry - 2)
+                carry = 1
+            else:
+                output = output + str(int(b[Bindex]) + carry)
+                carry = 0
+            Bindex = Bindex - 1
+            
+        if carry == 1:
+            output = output + "1"
+
+        reverseoutput = ""
+        i = len(output) - 1
+        while i >= 0:
+            reverseoutput = reverseoutput + output[i]
+            i = i - 1
+
+        return reverseoutput
+
+
+a = Solution()
+print(a.addBinary("11", "1"))
--- unit_test.py
+++ unit_test.py
@@ -9,5 +9,25 @@ class RemoveDuplicatesfromSortedArrayTest(unittest.TestCase):
         self.input2 = "1"
         self.assertEqual(temp.addBinary(self.input,self.input2), "100")
 
+    def test_basic_function2(self):
+        temp = Solution()
+        self.input = "1010"
+        self.input2 = "1011"
+        self.assertEqual(temp.addBinary(self.input,self.input2), "10101")
+
+    def test_basic_function3(self):
+        temp = Solution()
+        self.input = "111111"
+        self.input2 = "111111"
+        self.assertEqual(temp.addBinary(self.input,self.input2), "1111110")
+
+    def test_long_carry(self):
+        temp = Solution()
+        self.input = "1111111111111111111"
+        self.input2 = "1"
+        self.assertEqual(temp.addBinary(self.input,self.input2), "10000000000000000000")
+
+
+
 if __name__ == "__main__":
     unittest.main()
